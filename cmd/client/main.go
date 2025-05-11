c965f24714b0877927544e5dbb22f21dbaffe847
Iyadi Cyril Osita <iyadicy@gmail.com>
2025-05-11 00:42:32 -0400

    Add army moves queue declaration and subscription handling

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -40,15 +40,37 @@ func main() {
 		pubsub.Transient,
 	)
 	if err != nil {
-		log.Fatalf("Failed to declare and bind queue: %s\n", err)
+		log.Fatalf("Failed to declare and bind queue %s: %s\n", queueName, err)
 	}
 
 	fmt.Printf("Queue %s declared and bound\n", queueName)
+
+	armyMovesRoutingKey := fmt.Sprintf("%s.*", routing.ArmyMovesPrefix)
+	armyMovesQueue := fmt.Sprintf("%s.%s", routing.ArmyMovesPrefix, username)
+	armyQueuesRoutingKey := armyMovesQueue 
+
+	_, _, err = pubsub.DeclareAndBind(
+		conn,
+		routing.ExchangePerilTopic,
+		armyMovesQueue,
+		armyMovesRoutingKey,
+		pubsub.Transient,
+	)
+	if err != nil {
+		log.Fatalf("Failed to declare and bind queue %s: %s\n", queueName, err)
+	}
+
+	fmt.Printf("Queue %s declared and bound\n", armyMovesQueue)
 
 	// Create new game state
 	gameState := gamelogic.NewGameState(username)
 	err = pubsub.SubscribeJSON(conn, routing.ExchangePerilDirect, queueName, routing.PauseKey, pubsub.Transient, handlerPause(gameState))
 
+	if err != nil {
+		log.Fatalf("Failed to subscribe to queue: %s\n", err)
+	}
+
+	err = pubsub.SubscribeJSON(conn, routing.ExchangePerilTopic, armyMovesQueue, armyMovesRoutingKey, pubsub.Transient, handlerMove(gameState))
 	if err != nil {
 		log.Fatalf("Failed to subscribe to queue: %s\n", err)
 	}
@@ -81,12 +103,25 @@ func main() {
 				fmt.Println("Usage: move <location> <unit_id>")
 				continue
 			}
+
 			move, err := gameState.CommandMove(words)
 			if err != nil {
 				fmt.Printf("Error moving unit: %s\n", err)
 				continue
 			}
 			fmt.Printf("Units moved to %s\n", move.ToLocation)
+
+			channel, err := conn.Channel()
+			if err != nil {
+				log.Fatalf("Failed to open a channel: %s\n", err)
+			}
+
+			err = pubsub.PublishJSON(channel, routing.ExchangePerilTopic, armyQueuesRoutingKey, move)
+			if err != nil {
+				log.Fatalf("Failed to publish to exchange %s: %s\n", routing.ExchangePerilTopic, err)
+			}
+
+			fmt.Printf("Moves published to %s\n", armyMovesQueue)
 
 		case "status":
 			gameState.CommandStatus()
@@ -112,5 +147,13 @@ func handlerPause(gs *gamelogic.GameState) func(routing.PlayingState) {
 		defer fmt.Print("> ")
 
 		gs.HandlePause(ps)
+	}
+}
+
+func handlerMove(gs *gamelogic.GameState) func(gamelogic.ArmyMove) {
+	return func(am gamelogic.ArmyMove) {
+		defer fmt.Print("> ")
+
+		gs.HandleMove(am)
 	}
 }
