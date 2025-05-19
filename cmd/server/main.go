a831667f23f0ab5c49ebd52ec55173fce122342b
Iyadi Cyril Osita <iyadicy@gmail.com>
2025-05-19 00:13:50 -0400

    Add game log subscription to main server logic
    - Implemented subscription to game logs using PubSub, allowing for real-time logging of game events.
    - Added error handling for log writing failures, ensuring robust message processing.
    - Enhanced server output to indicate successful log subscriptions.

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -45,6 +45,21 @@ func main() {
 	}
 	fmt.Printf("Queue %s declared and bound\n", queueName)
 
+	// Subscribe to game logs
+	gameLogsRoutingKey := fmt.Sprintf("%s.*", routing.GameLogSlug)
+	err = pubsub.SubscribeGob(conn, routing.ExchangePerilTopic, routing.GameLogSlug, gameLogsRoutingKey, pubsub.Durable, func(log routing.GameLog) pubsub.AckType {
+		defer fmt.Print("> ")
+		err := gamelogic.WriteLog(log)
+		if err != nil {
+			fmt.Printf("Failed to write log: %v\n", err)
+			return pubsub.NackRequeue
+		}
+		return pubsub.Ack
+	})
+	if err != nil {
+		log.Fatalf("Failed to subscribe to game logs: %s\n", err)
+	}
+
 	gamelogic.PrintServerHelp()
 
 	for {
@@ -62,7 +77,7 @@ func main() {
 			err = pubsub.PublishJSON(channel, routing.ExchangePerilDirect, routing.PauseKey, routing.PlayingState{
 				IsPaused: true,
 			})
-		
+
 			if err != nil {
 				log.Printf("Failed to publish message: %s\n", err)
 				continue
