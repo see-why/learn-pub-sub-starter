10ea5a1d940b4427e320c731602fdde8ca38dd59
Iyadi Cyril Osita <iyadicy@gmail.com>
2025-05-19 23:49:26 -0400

    Remove unused handler functions from main.go
    - Deleted handlerPause, handlerMove, and handlerWar functions to streamline the codebase.
    - This cleanup improves maintainability by removing redundant logic related to game state management.

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"log"
 	"strings"
-	"time"
 
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
@@ -162,88 +161,6 @@ func main() {
 
 		default:
 			fmt.Println("Unknown command. Type 'help' for available commands.")
-		}
-	}
-}
-
-func handlerPause(gs *gamelogic.GameState) func(routing.PlayingState) pubsub.AckType {
-	return func(ps routing.PlayingState) pubsub.AckType {
-		defer fmt.Print("> ")
-
-		gs.HandlePause(ps)
-		return pubsub.Ack
-	}
-}
-
-func handlerMove(gs *gamelogic.GameState, channel *amqp.Channel) func(gamelogic.ArmyMove) pubsub.AckType {
-	return func(am gamelogic.ArmyMove) pubsub.AckType {
-		defer fmt.Print("> ")
-
-		outCome := gs.HandleMove(am)
-
-		switch outCome {
-		case gamelogic.MoveOutcomeSamePlayer:
-			return pubsub.NackDiscard
-		case gamelogic.MoveOutcomeMakeWar:
-			// Publish war message
-			warKey := fmt.Sprintf("%s.%s", routing.WarRecognitionsPrefix, gs.GetUsername())
-			err := pubsub.PublishJSON(
-				channel,
-				routing.ExchangePerilTopic,
-				warKey,
-				gamelogic.RecognitionOfWar{
-					Attacker: am.Player,
-					Defender: gs.GetPlayerSnap(),
-				},
-			)
-			if err != nil {
-				fmt.Printf("Failed to publish war message: %v\n", err)
-				return pubsub.NackRequeue
-			}
-			return pubsub.Ack
-		case gamelogic.MoveOutComeSafe:
-			return pubsub.Ack
-		default:
-			fmt.Println("error: unknown move outcome")
-			return pubsub.NackDiscard
-		}
-	}
-}
-
-func handlerWar(gs *gamelogic.GameState, channel *amqp.Channel) func(dw gamelogic.RecognitionOfWar) pubsub.AckType {
-	return func(dw gamelogic.RecognitionOfWar) pubsub.AckType {
-		defer fmt.Print("> ")
-
-		outcome, winner, loser := gs.HandleWar(dw)
-		switch outcome {
-		case gamelogic.WarOutcomeNotInvolved:
-			return pubsub.NackDiscard
-		case gamelogic.WarOutcomeNoUnits:
-			return pubsub.NackDiscard
-		case gamelogic.WarOutcomeOpponentWon, gamelogic.WarOutcomeYouWon, gamelogic.WarOutcomeDraw:
-			// Publish game log
-			var msg string
-			if outcome == gamelogic.WarOutcomeDraw {
-				msg = fmt.Sprintf("A war between %s and %s resulted in a draw", winner, loser)
-			} else {
-				msg = fmt.Sprintf("%s won a war against %s", winner, loser)
-			}
-
-			logEntry := routing.GameLog{
-				CurrentTime: time.Now(),
-				Message:     msg,
-				Username:    gs.Player.Username,
-			}
-			logKey := fmt.Sprintf("%s.%s", routing.GameLogSlug, gs.Player.Username)
-			err := pubsub.PublishGob(channel, routing.ExchangePerilTopic, logKey, logEntry)
-			if err != nil {
-				fmt.Printf("Failed to publish game log: %v\n", err)
-				return pubsub.NackRequeue
-			}
-			return pubsub.Ack
-		default:
-			fmt.Println("error: unknown war outcome")
-			return pubsub.NackDiscard
 		}
 	}
 }
